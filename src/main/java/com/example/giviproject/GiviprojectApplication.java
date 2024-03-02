f1252569d569c5460280d3ce92a09fa33aa77154
givexa3 <gugakhutsishvili@yahoo.com>
2024-03-02 15:53:59 +0400

    Initial Commit

---
+++ src/main/java/com/example/giviproject/GiviprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.giviproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiviprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiviprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/giviproject/GiviprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.giviproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiviprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
