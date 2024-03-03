a56f0ba7b1ed5ffcf83f523c292859fde41ae559
givexa3 <gugakhutsishvili@yahoo.com>
2024-03-03 23:36:43 +0400

    reimplement logic

--- src/main/java/com/example/giviproject/controller/UserController.java
+++ src/main/java/com/example/giviproject/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.example.giviproject.controller;
 
+import com.example.giviproject.dto.UserDTO;
 import com.example.giviproject.model.User;
 import com.example.giviproject.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//url for example /user should be here not on @GetMapping
 @RestController
 public class UserController {
 
@@ -16,9 +19,9 @@ public class UserController {
     }
 
     @GetMapping("/user")
-    public User getUser(@RequestParam Integer userId)
+    public ResponseEntity<UserDTO> getUser(@RequestParam Integer userId)
     {
         //late we need to reimplement logic to return UserDTO instead of user
-        return userService.getUserById(userId);
+        return ResponseEntity.ok(userService.getUserById(userId));
     }
 }
--- src/main/java/com/example/giviproject/exception/UserExceptionHandler.java
+++ src/main/java/com/example/giviproject/exception/UserExceptionHandler.java
@@ -17,6 +17,15 @@ public class UserExceptionHandler {
                 HttpStatus.NOT_FOUND
         );
 
+        //alternative also search what is the advantage and disadvantage of using Builder Pattern
+        //also write builder pattern from scratch by your own
+//        UserException userException1 = UserException
+//                .builder()
+//                .message(userNotFoundException.getMessage())
+//                .throwable(userNotFoundException.getCause())
+//                .httpStatus(HttpStatus.NOT_FOUND)
+//                .build();
+
         return new ResponseEntity<>(userException, HttpStatus.NOT_FOUND);
     }
 }
