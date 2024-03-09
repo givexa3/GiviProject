51e7d877b2a1bb195be84144fcb8d745178f9a50
givexa3 <gugakhutsishvili@yahoo.com>
2024-03-09 17:43:53 +0400

    remove extra import

--- src/main/java/com/example/giviproject/service/UserService.java
+++ src/main/java/com/example/giviproject/service/UserService.java
@@ -1,7 +1,6 @@
 package com.example.giviproject.service;
 
 import com.example.giviproject.dto.UserDTO;
-import com.example.giviproject.model.User;
 
 public interface UserService {
     UserDTO getUserById(Long id);
